41b304f884711448ea2536c89a4654933c6886f2
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-08-08 16:53:06 +0900

    Pull 실습

--- AlinicProject/Assets/aa.cs
+++ AlinicProject/Assets/aa.cs
@@ -7,7 +7,7 @@ public class aa : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //awadwd
+        
     }
 
     // Update is called once per frame

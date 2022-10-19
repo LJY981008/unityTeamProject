8141e3d561e9bb38511f50c9a42dc405b3424dc9
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-10-19 11:19:38 +0900

    피해받을때 이펙트 추가

--- AlinicProject/Assets/Scripts/Manager/UIManager.cs
+++ AlinicProject/Assets/Scripts/Manager/UIManager.cs
@@ -20,6 +20,7 @@ public class UIManager : MonoBehaviour
     public TextMeshProUGUI textBuffDuration;
     public Image bossHpFill;
     public Image bossHpBackground;
+    public Image imageDamageEffect;
 
     private float buffDuration;
     private Image selectWeapon;
@@ -28,6 +29,11 @@ public class UIManager : MonoBehaviour
     private float time;
     private float saveBossHpAmount;
     private float updateHpSpeed;
+    private bool isDamage;
+    private bool effectTrigger;
+    private float elapsed;
+    private float destAlpha;
+    private Color colorDamageSrc;
     private void Awake()
     {
         instance = this;
@@ -37,6 +43,11 @@ public class UIManager : MonoBehaviour
         updateHpSpeed = 0.3f;
         bodyGunImagePos = new Vector3(7.0f, 0.0f, 0.0f);
         bodyMagazineImagePos = new Vector3(7.0f, 35.0f, 0.0f);
+        colorDamageSrc = imageDamageEffect.color;
+        elapsed = 0.4f;
+        destAlpha = 0.1f;
+        isDamage = false;
+        effectTrigger = false;
     }
     private void Update()
     {
@@ -46,6 +57,10 @@ public class UIManager : MonoBehaviour
         else
         {
             imageBuffPanel.gameObject.SetActive(false);
+        }
+        if (isDamage)
+        {
+            DamageEffect();
         }
     }
     // 체력 증감 UI 적용
@@ -121,7 +136,31 @@ public class UIManager : MonoBehaviour
         {
             textBossName.gameObject.SetActive(false);
             saveBossHpAmount = bossHpFill.fillAmount;
+        }           
+    }
+    public void DamageEffect()
+    {
+        if (effectTrigger) 
+        {
+            elapsed *= -1;
+            effectTrigger = false;
         }
-                
+        colorDamageSrc.a += Time.deltaTime * elapsed;
+        imageDamageEffect.color = colorDamageSrc;
+        if (imageDamageEffect.color.a >= destAlpha)
+        {
+            effectTrigger = true;
+        }
+        if (imageDamageEffect.color.a <= 0)
+        {
+            colorDamageSrc.a = 0.0f;
+            elapsed *= -1;
+            imageDamageEffect.color = colorDamageSrc;
+            isDamage = false;
+        }
+    }
+    public void OnDamage()
+    {
+        isDamage = true;
     }
 }

46b6736368f030f5f88c19d93860f6f2b8e8d83d
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-12-18 18:21:37 +0900

    엔딩크래딧

---
+++ AlinicProject/Assets/Scripts/Manager/EndingManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class EndingManager : MonoBehaviour
+{
+    public static EndingManager instance;
+    public TextMeshProUGUI text;
+    Image panel;
+    Color tmpPanel;
+    Color zero;
+    Color tmpText;
+
+    private void Awake()
+    {
+        instance = this;
+        panel = transform.gameObject.GetComponent<Image>();
+        tmpPanel = panel.color;
+        zero = tmpPanel;
+        zero.a = 0;
+        panel.color = zero;
+
+        tmpText = text.color;
+        zero = tmpText;
+        zero.a = 0;
+        text.color = zero;
+    }
+    public void StartEvent()
+    {
+        StartCoroutine(UpdateAlpha());
+    }
+    IEnumerator UpdateAlpha()
+    {
+        while (panel.color.a <= 1)
+        {
+            tmpPanel.a += Time.deltaTime * 1f;
+            tmpText.a += Time.deltaTime * 1f;
+            panel.color = tmpPanel;
+            text.color = tmpText;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
